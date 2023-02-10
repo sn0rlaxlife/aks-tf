1bb4a208d4f88abaf58399f91b3b7947ce4dc6de
sn0rlax <79283899+sn0rlaxlife@users.noreply.github.com>
2023-02-10 13:47:47 -0800

    Update aks-cluster.tf

--- aks-cluster.tf
+++ aks-cluster.tf
@@ -21,7 +21,7 @@ resource "azurerm_kubernetes_cluster" "default" {
     location            = azurerm_resource_group.default.location
     resource_group_name = azurerm_resource_group.default.name
     dns_prefix          = "${random_pet.prefix.id}-k8s"
-
+ 
 
     default_node_pool {
         name            = "default"
